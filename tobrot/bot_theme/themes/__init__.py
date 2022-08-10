1a9326023b7532e854cc6433fb53aeeb415ec0ac
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-08-10 13:58:39 +0530

    Update and rename tobrot/bot_theme/bot_UI.py to tobrot/bot_theme/themes/__init__.py

---
+++ tobrot/bot_theme/themes/__init__.py
@@ -0,0 +1,9 @@
+from tobrot import BOT_THEME
+from tobrot.bot_theme.theme import TXStyle
+
+if BOT_THEME == "fx-optimized-theme":
+    TXStyle()
+elif BOT_THEME == "fx-minimal-theme":
+    TXStyle()
+elif BOT_THEME == "fx-destructive-theme":
+    TXStyle()
