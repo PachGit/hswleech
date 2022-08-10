e135bfcaf5c7734bb5eabe2ccc5c66036b7a7380
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-08-10 14:18:02 +0530

    Update __init__.py

--- tobrot/__init__.py
+++ tobrot/__init__.py
@@ -223,7 +223,7 @@ PRM_USERS = getVar("PRM_USERS", "") #Optional
 PRM_LOG = getVar("PRM_LOG", "") #Optional 
 
 # Bot Theme [ UI & Customization ] >>>>>>>>
-BOT_THEME = getVar("BOT_THEME", "fx-optimised")
+BOT_THEME = getVar("BOT_THEME", "fx-optimised-theme")
 
 # ForceSubscribe [ Channel ] >>>>>>>>
 FSUB_CHANNEL = getVar("FSUB_CHANNEL", "") #Do Not Put this Now
