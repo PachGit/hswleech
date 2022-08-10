4611095b169500b3516e5e514f6ab1a762c5cadf
MysterySD <80155750+5MysterySD@users.noreply.github.com>
2022-08-10 14:19:15 +0530

    Update __init__.py

--- tobrot/bot_theme/themes/__init__.py
+++ tobrot/bot_theme/themes/__init__.py
@@ -1,9 +1,10 @@
 from tobrot import BOT_THEME
 from tobrot.bot_theme.themes import fx_optimised
 
-if BOT_THEME == "fx-optimized-theme":
-    fx_optimised.TXStyle()
-elif BOT_THEME == "fx-minimal-theme":
-    TXStyle()
-elif BOT_THEME == "fx-destructive-theme":
-    TXStyle()
+def BotTheme():
+    if BOT_THEME == "fx-optimized-theme":
+        return fx_optimised.TXStyle()
+    elif BOT_THEME == "fx-minimal-theme":
+        TXStyle()
+    elif BOT_THEME == "fx-destructive-theme":
+        TXStyle()
